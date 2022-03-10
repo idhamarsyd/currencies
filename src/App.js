b8c39daa8c9155379ac1baf2631402a2d81b42cc
idhamarsyd <idham_arrasyid@outlook.com>
2022-03-10 21:54:41 +0800

    fetch & display data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { React, useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [currencies, setCurrencies] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(
+  //     "https://api.currencyfreaks.com/latest?apikey=ea1be70744ce4473b77c2988cc9ba717"
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setCurrencies(data.rates))
+  //     .catch((err) => console.log(err))
+  //     .finally(() => setIsLoading(false));
+  // }, []);
+  const displayData = (data) => {
+    const names = Object.keys(data);
+    const rates = Object.values(data);
+    const setData = names.map((name, index) => {
+      return (
+        <tr>
+          <td>{name}</td>
+          <td>{rates[index]}</td>
+        </tr>
+      );
+    });
+    return setData;
+  };
+
+  useEffect(() => {
+    fetch(
+      "https://api.currencyfreaks.com/latest?apikey=ea1be70744ce4473b77c2988cc9ba717&symbols=CAD,IDR,JPY,CHF,EUR,GBP"
+    )
+      .then((response) => response.json())
+      .then((response) => setCurrencies(response.rates))
+      .catch((err) => console.log("Ooops ada Error!"));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Assignment 2 Currencies</h1>
+      <ul></ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Currencies</th>
+          </tr>
+        </thead>
+        <tbody>{displayData(currencies)}</tbody>
+      </table>
     </div>
   );
-}
-
+};
 export default App;
