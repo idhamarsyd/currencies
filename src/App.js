1d79523f1c38aa21351d73f11a54a832b11dfcae
idhamarsyd <idham_arrasyid@outlook.com>
2022-03-10 22:12:20 +0800

    added exchange rate, selling rate

--- src/App.js
+++ src/App.js
@@ -6,24 +6,28 @@ const App = () => {
   const [currencies, setCurrencies] = useState({});
   const [isLoading, setIsLoading] = useState(false);
 
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   fetch(
-  //     "https://api.currencyfreaks.com/latest?apikey=ea1be70744ce4473b77c2988cc9ba717"
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => setCurrencies(data.rates))
-  //     .catch((err) => console.log(err))
-  //     .finally(() => setIsLoading(false));
-  // }, []);
   const displayData = (data) => {
     const names = Object.keys(data);
     const rates = Object.values(data);
+    console.log(rates);
+
+    const buyRates = rates.map((rate) => {
+      const rateValue = parseFloat(rate) + parseFloat(rate) * (5 / 100);
+      return rateValue.toFixed(4);
+    });
+
+    const sellRates = rates.map((rate) => {
+      const rateValue = parseFloat(rate) - parseFloat(rate) * (5 / 100);
+      return rateValue.toFixed(4);
+    });
+    // console.log(sellRates);
     const setData = names.map((name, index) => {
       return (
         <tr>
           <td>{name}</td>
+          <td>{buyRates[index]}</td>
           <td>{rates[index]}</td>
+          <td>{sellRates[index]}</td>
         </tr>
       );
     });
@@ -46,8 +50,10 @@ const App = () => {
       <table>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Currencies</th>
+            <th>Currency</th>
+            <th>We Buy </th>
+            <th>Exchange Rate</th>
+            <th>We Sell</th>
           </tr>
         </thead>
         <tbody>{displayData(currencies)}</tbody>
